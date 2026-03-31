0e43416897769246c609c34341d05962a2edb0ac
mavejpeg <91692420+mavejpeg@users.noreply.github.com>
2026-03-31 23:56:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1183,6 +1183,10 @@ app.delete('/api/snus-lines/:id', requireRop, async (req, res) => {
     }
 });
 
+app.get('/editor.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'editor.js'));
+});
+
 // ========== START SERVER ==========
 async function startServer() {
     console.log('\n🚀 Starting server...\n');
